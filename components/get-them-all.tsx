a3fe52e385adaa311acaae4cb344a0833dce7d8a
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-10-05 21:21:24 +0100

    feat: Added Get them all sectioN

---
+++ components/get-them-all.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react'
+
+export const GetThemAll: FC = () => {
+  return (
+    <div className="GetThemAll">
+      <div className="content">
+        <h1>
+          Get Them All
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laboriosam fugit error obcaecati dicta officiis, fugiat est pariatur corrupti vitae laborum voluptas vel perferendis, rem tempore incidunt aut, autem culpa!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium ab mollitia earum repellat doloremque eum! Amet possimus dolore aut animi perferendis impedit, quaerat nemo temporibus cum tenetur aspernatur quo.
+        </p>
+      </div>
+      <div className="robot-image" />
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
+import {GetThemAll} from '../components/get-them-all'
 
 const Home: NextPage = () => {
   return (
-    <div className="container py-2 bg-primary-400 intro">
-      
-    <video autoPlay muted loop id="myVideo">
-      <source src="/55-face.mp4" type="video/mp4" />
-    </video>
+    <div className="container py-2 bg-primary-400 intro pt-0">
+        
+      {/* <video autoPlay muted loop id="myVideo">
+        <source src="/55-face.mp4" type="video/mp4" />
+      </video> */}
 
+
+    <GetThemAll />
     </div>
   )
 }
 
 export default Home
-  
