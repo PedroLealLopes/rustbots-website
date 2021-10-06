9d3722d41142e6e15def45e2f2ec4390243411f2
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-10-06 18:43:25 +0100

    test: husky again

--- components/get-them-all.tsx
+++ components/get-them-all.tsx
@@ -9,7 +9,7 @@ export const GetThemAll: FC = () => {
         </h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laboriosam fugit error obcaecati dicta officiis, fugiat est pariatur corrupti vitae laborum voluptas vel perferendis, rem tempore incidunt aut, autem culpa!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium ab mollitia earum repellat doloremque eum! Amet possimus dolore aut animi perferendis impedit, quaerat nemo temporibus cum tenetur aspernatur quo
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium ab mollitia earum repellat doloremque eum! Amet possimus dolore aut animi perferendis impedit, quaerat nemo temporibus cum tenetur aspernatur quo.
         </p>
       </div>
       <div className="robot-image" />
