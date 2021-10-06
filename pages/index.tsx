d13af137e5a467f94243fe3c8ef519119b9fb0de
Rjribeiro99 <ricardojsribeiro99@gmail.com>
2021-10-06 18:52:14 +0100

    @media only screen changes
    @media only screen changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import {GetThemAll} from '../components/get-them-all'
 import {Partners} from '../components/partners'
+import {MeetTheTeam} from '../components/meet-the-team'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
 
     <GetThemAll />
     <Partners />
+    <MeetTheTeam />
     </div>
   )
 }
