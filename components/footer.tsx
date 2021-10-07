062c22a88a4570abddec2659dd8eff6ba9889e94
Rjribeiro99 <ricardojsribeiro99@gmail.com>
2021-10-07 20:07:20 +0100

    Footer and Roadmap Mobile Optimization
    Footer and Roadmap Mobile Optimization

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,13 @@ export const Footer: FC = () => {
     <div className="Footer">
       <div className="content">
         <h1>THATS ALL FOLKS!</h1>
+        
         <button>Magic Eden</button>
         <button>Connect Wallet</button>
         <button>Twitter</button>
         <button>Insta</button>
         <button>Discord</button>
+        <p> © All Rights Reserved 2021 </p>
       </div>
       <div className="footer-image" />
     </div>
