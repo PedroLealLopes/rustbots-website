1090b83e3f74561fedee5482fa9801d89252340c
Rjribeiro99 <ricardojsribeiro99@gmail.com>
2021-10-07 19:47:21 +0100

    Footer / Buttons not done
    Footer / Buttons not done

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from 'react'
+
+export const Footer: FC = () => {
+  return (
+    <div className="Footer">
+      <div className="content">
+        <h1>THATS ALL FOLKS!</h1>
+        <button>Magic Eden</button>
+        <button>Connect Wallet</button>
+        <button>Twitter</button>
+        <button>Insta</button>
+        <button>Discord</button>
+      </div>
+      <div className="footer-image" />
+    </div>
+  )
+}
+
+
