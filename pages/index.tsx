b986dda29cd4b80c89a111ef48fdfce1f6f1eb21
Rjribeiro99 <ricardojsribeiro99@gmail.com>
2021-10-07 19:47:59 +0100

    Roadmap
    Roadmap

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import type { NextPage } from 'next'
 import {GetThemAll} from '../components/get-them-all'
 import {Partners} from '../components/partners'
 import {MeetTheTeam} from '../components/meet-the-team'
+import { Footer } from '../components/footer'
+import { Roadmap } from '../components/roadmap'
 
 const Home: NextPage = () => {
   return (
-    <div className="container py-2 bg-primary-400 intro pt-0">
+    <div className="container py-0 bg-primary-400 intro pt-0">
         
       {/* <video autoPlay muted loop id="myVideo">
         <source src="/55-face.mp4" type="video/mp4" />
@@ -13,9 +15,10 @@ const Home: NextPage = () => {
 
 
     <GetThemAll />
+    <Roadmap />
     <MeetTheTeam />
     <Partners />
-
+    <Footer />  
     </div>
   )
 }
