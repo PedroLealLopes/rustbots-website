e1834cd078ce4e59fa6cf89878ccf47e4f7c118d
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-10-08 19:23:23 +0100

    fix: added buttons

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,21 @@
 import React, {FC} from 'react'
+import { Button } from './button'
 
 export const Footer: FC = () => {
   return (
     <div className="Footer">
       <div className="content">
         <h1>THATS ALL FOLKS!</h1>
-        <button>Magic Eden</button>
-        <button>Connect Wallet</button>
-        <button>Twitter</button>
-        <button>Insta</button>
-        <button>Discord</button>
+        
+        <div className="button-list" style={{ width: "100%" }}>
+          <Button Title="MAGIC EDEN" Href="#" />
+          <Button Title="CONNECT WALLET"  Href="#"/>
+          <div className="button-list-socials">
+            <Button Social="twitter" />
+            <Button Social="instagram" />
+            <Button Social="discord" />
+          </div>
+        </div>
       </div>
       <div className="footer-image" />
     </div>
