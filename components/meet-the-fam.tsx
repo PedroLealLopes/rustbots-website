1ba3a5d1d32e278368690d5e79ede9a978bb075a
Rjribeiro99 <ricardojsribeiro99@gmail.com>
2021-10-08 20:00:36 +0100

    Meet The Fam Section
    Meet The Fam Section

---
+++ components/meet-the-fam.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from 'react'
+
+export const MeetTheFam: FC = () => {
+  return (
+    <div className="meet-the-fam">
+      <div className="content">
+        <h1>MEET THE FAM</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laboriosam fugit error obcaecati dicta officiis, fugiat est pariatur corrupti vitae laborum voluptas vel perferendis, rem tempore incidunt aut, autem culpa!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium ab mollitia earum repellat doloremque eum! Amet possimus dolore aut animi perferendis impedit, quaerat nemo temporibus cum tenetur aspernatur quo.
+        </p>
+      </div>
+      <div className="fam-slider">
+        <div className="fam-image">
+          SLIDER PLACEHOLDER // WIDTH // HEIGHT
+        </div>
+        <div className="fam-slider-info">
+          <h2>ROBOT NAME</h2>
+          <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium ab mollitia earum repellat doloremque eum! Amet possimus dolore aut animi perferendis impedit, quaerat nemo temporibus cum tenetur aspernatur quo.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import {Partners} from '../components/partners'
 import {MeetTheTeam} from '../components/meet-the-team'
 import {Intro} from '../components/intro'
 import {Footer} from '../components/footer'
+import { MeetTheFam } from '../components/meet-the-fam'
 
 const Home: NextPage = () => {
   return (
     <div className="container py-2 pt-0">
       <Intro />
       <GetThemAll />
+      <MeetTheFam />
       <MeetTheTeam />
       <Partners />
       <Footer />
