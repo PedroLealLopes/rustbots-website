27bb987aa3aaf70724440e7281e32214bb9934e8
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-10-10 02:48:20 +0100

    Feat: Added Meet the Fam slider

--- components/meet-the-fam.tsx
+++ components/meet-the-fam.tsx
@@ -1,25 +1,130 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
+
+import dynamic from 'next/dynamic';
+
+const Carousel = dynamic(
+  () => import ('@brainhubeu/react-carousel'),
+  {
+  ssr: false
+  }
+)
+
+import { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+
+const imagesTest = ["/bot00.png", "/bot01.png", "/bot02.png", "/bot03.png", "/bot04.png"];
 
 export const MeetTheFam: FC = () => {
+  const [current, setCurrent] = useState(0)
+
+  
+  const onChange = (value) => {
+    if(value === -1)
+      value = 4
+    if(value === 5)
+      value = 0
+    setCurrent(value)
+  }
+
+  const botDescription = (val) => {
+    switch (val) {
+      case 0:
+        return (
+          <div>
+            <h3 className="text-2xl font-bold mb-2">CanBot v1.0 - Personal Assistant - Sebastion</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias et dolore quas accusamus, esse placeat assumenda distinctio. Odio distinctio a quisquam, voluptatem sint, voluptas pariatur inventore, provident dolorem voluptatibus repellat.</p>
+          </div>
+        )
+        break;
+      case 1:
+        return (
+          <div>
+            <h3 className="text-2xl font-bold mb-2">CanBot v2.0 - Personal Assistant - Sebastion</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias et dolore quas accusamus, esse placeat assumenda distinctio. Odio distinctio a quisquam, voluptatem sint, voluptas pariatur inventore, provident dolorem voluptatibus repellat.</p>
+          </div>
+        )
+        break;
+      case 2:
+        return (
+          <div>
+            <h3 className="text-2xl font-bold mb-2">CanBot v3.0 - Personal Assistant - Sebastion</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias et dolore quas accusamus, esse placeat assumenda distinctio. Odio distinctio a quisquam, voluptatem sint, voluptas pariatur inventore, provident dolorem voluptatibus repellat.</p>
+          </div>
+        )
+        break;
+      case 3:
+        return (
+          <div>
+            <h3 className="text-2xl font-bold mb-2">CanBot v4.0 - Personal Assistant - Sebastion</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias et dolore quas accusamus, esse placeat assumenda distinctio. Odio distinctio a quisquam, voluptatem sint, voluptas pariatur inventore, provident dolorem voluptatibus repellat.</p>
+          </div>
+        )
+        break;
+      case 4:
+        return (
+          <div>
+            <h3 className="text-2xl font-bold mb-2">CanBot v5.0 - Personal Assistant - Sebastion</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias et dolore quas accusamus, esse placeat assumenda distinctio. Odio distinctio a quisquam, voluptatem sint, voluptas pariatur inventore, provident dolorem voluptatibus repellat.</p>
+          </div>
+        )
+        break;
+    }
+  }
+
   return (
     <div className="meet-the-fam">
-      <div className="content">
+      <div className="content mb-8 sm:mb-16">
         <h1>MEET THE FAM</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laboriosam fugit error obcaecati dicta officiis, fugiat est pariatur corrupti vitae laborum voluptas vel perferendis, rem tempore incidunt aut, autem culpa!
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium ab mollitia earum repellat doloremque eum! Amet possimus dolore aut animi perferendis impedit, quaerat nemo temporibus cum tenetur aspernatur quo.
         </p>
       </div>
-      <div className="fam-slider">
-        <div className="fam-image">
-          SLIDER PLACEHOLDER // WIDTH // HEIGHT
-        </div>
-        <div className="fam-slider-info">
-          <h2>ROBOT NAME</h2>
-          <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium ab mollitia earum repellat doloremque eum! Amet possimus dolore aut animi perferendis impedit, quaerat nemo temporibus cum tenetur aspernatur quo.</p>
-        </div>
+
+      <Carousel
+        value={current}
+        onChange={onChange}
+          plugins={[
+            'centered',
+            'infinite',
+            'fastSwipe',
+          {
+            resolve: slidesToShowPlugin,
+            options: {
+            numberOfSlides: 2,
+            },
+          },
+        ]}   
+
+              
+        breakpoints={{
+          1024: {
+            plugins: [
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 1
+                }
+              },
+            ]
+          }
+        }}
+      >
+        {
+          imagesTest.map((img, index) => {
+            return (
+              <img key={index} src={img} style={{ width: "500px" }} alt="" />
+            )
+          })
+        }
+      </Carousel>
+        
+      
+      <div className="w-full sm:w-1/2">
+        {botDescription(current)}
       </div>
+
+      
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,16 @@ import {Roadmap} from '../components/roadmap'
 import {Intro} from '../components/intro'
 import {Footer} from '../components/footer'
 import { MeetTheFam } from '../components/meet-the-fam'
+import Head from "next/head"
 
 const Home: NextPage = () => {
   return (
     <div className="container py-2 pt-0 pb-0">
+      <Head>
+        <title>RustBots Home 🤖</title>
+        <meta property="og:title" content="RustBots Home 🤖" key="title" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous" />
+      </Head>
       <Intro />
       <GetThemAll />
       <MeetTheFam />
